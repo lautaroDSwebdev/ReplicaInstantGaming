1dda2ece8256b15da963a82b3f12de584cbe21ac
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2025-01-11 14:20:32 -0300

    add / adding images of homepage #id5434

---
+++ src/mock/data.ts
@@ -0,0 +1,23 @@
+const data = {
+    navbarTop: [
+        {
+            id: 54543,
+            url: "Tendencias" 
+        },
+        {
+            id: 43545,
+            url: "Reservas" 
+        },
+        {
+            id: 43545,
+            url: "Proximas salidas" 
+        },
+        {
+            id: 43545,
+            url: "Soporte 24/7" 
+        }
+    ]
+}
+
+
+
