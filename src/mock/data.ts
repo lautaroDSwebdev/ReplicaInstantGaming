7284166f64200024f83c3e0d453f9e920b4c9f5d
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2025-01-12 09:13:47 -0300

    general_purpose_: section tendenies + global classes + multimedia

--- src/mock/data.ts
+++ src/mock/data.ts
@@ -1,21 +1,69 @@
-const data = {
-    navbarTop: [
+export const data = {
+    Tendencies: [
         {
             id: 54543,
-            url: "Tendencias" 
+            title: "Murky Divers",
+            price: 3.22  ,
+            discount: 39,
+            img: "./murky-divers-pc-juego-steam-cover.png",
+            video: "./murky-divers-pc-juego-steam-preview.webm"
         },
         {
             id: 43545,
-            url: "Reservas" 
+            title: "Farming Simulator 25",
+            price:  30.75,
+            discount: 39,
+            img: "./farming-simulator-25-pc-mac-juego-steam-cover.png"
         },
         {
             id: 43545,
-            url: "Proximas salidas" 
+            title: "Sengoku Dynasty",
+            price: 19.25,
+            discount: 39,
+            img: "./sengoku-dynasty-pc-juego-steam-cover.png"
         },
         {
             id: 43545,
-            url: "Soporte 24/7" 
-        }
+            title: "Los Sims 4 Vida y Más Allá",
+            price:  29.57,
+            discount: 39,
+            img: "./los-sims-4-vida-y-mas-alla-pc-juego-ea-app-cover.png"
+        },
+        {
+            id: 5656,
+            title: "Microsoft Flight Simulator 2024 (PC / Xbox Serie",
+            price:  49.72,
+            discount: 39,
+            img: "./microsoft-flight-simulator-2024-pc-xbox-series-x-s-pc-xbox-series-x-s-juego-microsoft-store-cover.png"
+        },
+        {
+            id: 6757,
+            title: "Neva",
+            price:  14.65,
+            discount: 39,
+            img: "./neva-pc-mac-juego-steam-cover.png"
+        },
+        {
+            id: 87887,
+            title: "No More Room In Hell 2",
+            price:  19.99,
+            discount: 39,
+            img: "./no-more-room-in-hell-2-pc-juego-steam-cover.png"
+        },
+        {
+            id: 6768757,
+            title: "Farming Simulator 25 - MacDon Pack",
+            price:  3.99,
+            discount: 39,
+            img: "./farming-simulator-25-pc-mac-juego-steam-cover.png"
+        },
+        {
+            id: 8798,
+            title: "Sea Power : Naval Combat in the Missile Age",
+            price:  34.86,
+            discount: 39,
+            img: "./sea-power-naval-combat-in-the-missile-age-pc-juego-steam-cover.png"
+        },
     ]
 }
 
